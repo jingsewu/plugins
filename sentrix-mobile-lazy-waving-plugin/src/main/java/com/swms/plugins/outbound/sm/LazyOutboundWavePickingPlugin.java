74098ec6d208f2ca19574b5a209842aeb8bb1787
Church <church@mail.com>
2024-03-14 11:24:45 +0800

    feat: sentrix mobile lazy waving

--- sentrix-mobile-lazy-waving-plugin/src/main/java/com/swms/plugins/outbound/sm/LazyOutboundWavePickingPlugin.java
+++ sentrix-mobile-lazy-waving-plugin/src/main/java/com/swms/plugins/outbound/sm/LazyOutboundWavePickingPlugin.java
@@ -5,6 +5,7 @@ import com.swms.wms.api.outbound.IOutboundPlanOrderApi;
 import com.swms.wms.api.outbound.constants.OutboundPlanOrderStatusEnum;
 import com.swms.wms.api.outbound.dto.OutboundPlanOrderDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.pf4j.Extension;
@@ -16,6 +17,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Extension
 @RequiredArgsConstructor
 public class LazyOutboundWavePickingPlugin implements IOutboundWavePickingPlugin {
@@ -24,6 +26,8 @@ public class LazyOutboundWavePickingPlugin implements IOutboundWavePickingPlugin
 
     @Override
     public List<List<OutboundPlanOrderDTO>> doOperation(List<OutboundPlanOrderDTO> originalOutboundPlanOrders) {
+        log.debug("Receive lazy waving request, original outbound plan orders size: {}", originalOutboundPlanOrders.size());
+
         Set<String> customerWaveNos = originalOutboundPlanOrders.stream()
             .map(OutboundPlanOrderDTO::getCustomerWaveNo).collect(Collectors.toSet());
         List<OutboundPlanOrderDTO> outboundPlanOrderDTOS = outboundPlanOrderApi.findByCustomerWaveNos(customerWaveNos);
@@ -31,14 +35,32 @@ public class LazyOutboundWavePickingPlugin implements IOutboundWavePickingPlugin
         Map<String, List<OutboundPlanOrderDTO>> outboundPlanOrderGroupMap = outboundPlanOrderDTOS.stream()
             .collect(Collectors.groupingBy(OutboundPlanOrderDTO::getCustomerWaveNo));
 
-        boolean recheckStationIsOnline = recheckStationIsOnline();
-
         // 复核打包台已上线，或者订单波次没有已完成订单，否则就过滤掉，等待打包台上线
-        List<OutboundPlanOrderDTO> outboundPlanOrders = originalOutboundPlanOrders.stream().filter(v ->
-            recheckStationIsOnline || outboundPlanOrderGroupMap.get(v.getCustomerWaveNo()).stream()
-                .noneMatch(dto -> OutboundPlanOrderStatusEnum.isFinalStatues(dto.getOutboundPlanOrderStatus()))
+        List<OutboundPlanOrderDTO> outboundPlanOrders = originalOutboundPlanOrders.stream()
+            .filter(v -> outboundPlanOrderGroupMap.get(v.getCustomerWaveNo()).stream()
+                .noneMatch(dto -> OutboundPlanOrderStatusEnum.NEW != dto.getOutboundPlanOrderStatus())
         ).toList();
 
+        log.debug("After filter, new outbound plan orders size: {}", outboundPlanOrders.size());
+
+        // 如果原始单量和过滤后的单量不一致，说明有父单完成了，就需要判断复核打包台的状态了
+        if (originalOutboundPlanOrders.size() != outboundPlanOrders.size()) {
+            boolean recheckStationIsOnline = recheckStationIsOnline();
+            // 复核打包台已上线，就发全部的订单，否则只发父单没有完成的订单
+            return waving(recheckStationIsOnline ? originalOutboundPlanOrders : outboundPlanOrders);
+        }
+
+        return waving(outboundPlanOrders);
+    }
+
+    private boolean recheckStationIsOnline() {
+        boolean isOnline = false;
+        log.debug("Recheck station online status is : {}", isOnline);
+        return isOnline;
+    }
+
+    private List<List<OutboundPlanOrderDTO>> waving(List<OutboundPlanOrderDTO> outboundPlanOrders) {
+        log.debug("Waving outbound plan orders size: {}", outboundPlanOrders.size());
         // 外部波次号为空的订单单组独成为一个波次
         List<List<OutboundPlanOrderDTO>> emptyWaveNoOrders = outboundPlanOrders.stream()
             .filter(order -> StringUtils.isEmpty(order.getCustomerWaveNo()))
@@ -50,10 +72,8 @@ public class LazyOutboundWavePickingPlugin implements IOutboundWavePickingPlugin
             .collect(Collectors.groupingBy(OutboundPlanOrderDTO::getCustomerWaveNo));
 
         Collection<List<OutboundPlanOrderDTO>> results = CollectionUtils.union(emptyWaveNoOrders, outboundWaveMap.values());
+        log.debug("Waving success! empty waving no orders size: {}, has customer wave no plan orders size: {}",
+            emptyWaveNoOrders.size(), outboundWaveMap.values().size());
         return new ArrayList<>(results);
-    }
-
-    private boolean recheckStationIsOnline() {
-        return false;
     }
 }
